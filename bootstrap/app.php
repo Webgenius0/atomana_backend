465a62f0593a3805777317850c51e78e30bdb769
mdiktushar <md.tushar1804@gmail.com>
2025-02-07 15:13:40 +0600

    fix route folder for api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -45,7 +45,7 @@ use Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api/v1/api.php',
+        api: __DIR__ . '/../routes/api/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
 

9d73eb58f6047bcc096f736f273b966b5e8c184a
mdiktushar <md.tushar1804@gmail.com>
2025-02-07 16:52:35 +0600

    profile route setup

--- app/Http/Middleware/Authorized.php
+++ app/Http/Middleware/Authorized.php
@@ -6,6 +6,7 @@ use App\Traits\V1\ApiResponse;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class Authorized
@@ -19,7 +20,7 @@ class Authorized
     public function handle(Request $request, Closure $next): Response
     {
         $user = Auth::user();
-        if ($user && in_array(optional($user->role)->name, ['agent', 'super admin'])) {
+        if ($user && in_array(optional($user->role)->name, ['agent', 'admin'])) {
             return $next($request);
         }
         return $this->error(403, 'Only Admins & Agents are Allowed to Access');
--- routes/api/v1/profile/profile.php
+++ routes/api/v1/profile/profile.php
@@ -1,12 +1,15 @@
 <?php
 
+use App\Http\Controllers\API\V1\Profile\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 // all profile route of profile
 Route::prefix('/profile')->name('profile.')->middleware(['auth:api'])->group(function () {
 
     // routes for bouth admin and agents
-    Route::middleware(['authorized'])->group(function () {});
+    Route::middleware(['authorized'])->controller(ProfileController::class)->group(function () {
+        Route::get('/show', 'show')->name('show');
+    });
     // route for admin
     Route::middleware(['admin'])->group(function () {});
     // route for agent

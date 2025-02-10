65d637899269c5fb23f53848be89fb0ec19214d6
mdiktushar <md.tushar1804@gmail.com>
2025-02-10 15:25:34 +0600

    profile index created

--- app/Http/Resources/API/V1/Admin/AgentIndexResource.php
+++ app/Http/Resources/API/V1/Admin/AgentIndexResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\API\V1\Admin;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Log;
 
 class AgentIndexResource extends JsonResource
 {
@@ -14,6 +15,20 @@ class AgentIndexResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        $datas = parent::toArray($request);
+        $agents = [];
+        foreach($datas['data'] as $data) {
+            $agents[] = [
+                'id' => $data['id'],
+                'first_name' => $data['first_name'],
+                'last_name' => $data['last_name'],
+                'email' => $data['email'],
+                'avatar' => $data['avatar'],
+                'phone' => $data['profile']['phone'],
+                'role' => $data['role']['name'],
+            ];
+        }
+        $datas['data'] = $agents;
+        return $datas;
     }
 }

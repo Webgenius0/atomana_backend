05235cc13ec011c5adf064d14c37e6cced7576f3
mdiktushar <md.tushar1804@gmail.com>
2025-02-10 14:25:27 +0600

    admin agent route

--- routes/api/api.php
+++ routes/api/api.php
@@ -8,3 +8,4 @@ use Illuminate\Support\Facades\Route;
  */
 require 'v1/auth/auth.php';    // All Auth routes
 require 'v1/profile/profile.php';    // All Auth routes
+require 'v1/admin/admin.php';    // All admin routes
---
+++ routes/api/v1/admin/admin.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\API\V1\Admin\AgentController;
+use Illuminate\Support\Facades\Route;
+
+//routes for bouth admin
+Route::prefix('/admin')->name('admin.')->middleware(['auth:api', 'admin'])->group(function () {
+    Route::prefix('/agent')->name('agent.')->controller(AgentController::class)->group(function () {
+        Route::get('/', 'index')->name('index');
+    });
+});

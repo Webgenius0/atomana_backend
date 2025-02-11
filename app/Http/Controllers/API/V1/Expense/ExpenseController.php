0abd56e3b645498e7e91158522cb3c266f7d69d7
mdiktushar <md.tushar1804@gmail.com>
2025-02-11 17:39:53 +0600

    expense controller

---
+++ app/Http/Controllers/API/V1/Expense/ExpenseController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\API\V1\Expense;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\API\V1\Expense\CreateExpenseRequest;
+use App\Services\API\V1\Expense\ExpenseService;
+use App\Traits\V1\ApiResponse;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class ExpenseController extends Controller
+{
+    use ApiResponse;
+    protected ExpenseService $expenseService;
+
+    public function __construct(ExpenseService $expenseService)
+    {
+        $this->expenseService = $expenseService;
+    }
+
+    public function store(CreateExpenseRequest $createExpenseRequest)
+    {
+        try{
+            
+        }catch(Exception $e) {
+            Log::error('ExpenseController::store', ['error' => $e->getMessage()]);
+            return $this->error(500, 'Server Error', $e->getMessage());
+        }
+    }
+
+}

dfac92d55f31606f4526950506d4e57fffbb612e
mdiktushar <md.tushar1804@gmail.com>
2025-02-13 09:33:13 +0600

    commad route update

--- routes/api/v1/command/command.php
+++ routes/api/v1/command/command.php
@@ -1,48 +1,49 @@
 <?php
 
 use App\Helpers\Helper;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
 
 // Clear all cache and optimize the application
-Route::get('/v1/optimize-clear', function () {
+Route::get('/v1/optimize-clear', function (): JsonResponse {
     Artisan::call('optimize:clear');
     return Helper::success(200, 'Cache cleared and application optimized successfully.');
 });
 
 // Run migrations
-Route::get('/v1/migrate', function () {
+Route::get('/v1/migrate', function () : JsonResponse{
     Artisan::call('migrate');
     return Helper::success(200, 'Migrations ran successfully.');
 });
 
 // Run fresh migrations (drop all tables and run migrations again)
-Route::get('/v1/migrate-fresh', function () {
+Route::get('/v1/migrate-fresh', function (): JsonResponse {
     Artisan::call('migrate:fresh');
     return Helper::success(200, 'Database refreshed and migrations run successfully.');
 });
 
 // Run fresh migrations and seed the database
-Route::get('/v1/migrate-fresh-seed', function () {
+Route::get('/v1/migrate-fresh-seed', function () : JsonResponse{
     Artisan::call('migrate:fresh --seed');
     return Helper::success(200, 'Database refreshed, migrations run, and seeding completed successfully.');
 });
 
 // Seed the database
-Route::get('/v1/seed', function () {
+Route::get('/v1/seed', function () : JsonResponse{
     Artisan::call('db:seed');
     return Helper::success(200, 'Database seeding completed successfully.');
 });
 
 //queue-work
-Route::get('/v1/queue-work', function () {
+Route::get('/v1/queue-work', function (): JsonResponse {
     Artisan::call('queue:work --stop-when-empty');
     return Helper::success(200, 'Queue worker started successfully.');
 });
 
 
 // Run composer update
-Route::get('/v1/composer-update', function () {
+Route::get('/v1/composer-update', function () : JsonResponse{
     $output = shell_exec('composer update 2>&1');
     return Helper::success(200, 'Composer update completed.', $output);
 });

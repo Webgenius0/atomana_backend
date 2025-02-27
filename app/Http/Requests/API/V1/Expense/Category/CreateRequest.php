85e7eda9176ba0994330ca371d9df39e1d2dd31e
mdiktushar <md.tushar1804@gmail.com>
2025-02-27 11:39:57 +0600

    request created

---
+++ app/Http/Requests/API/V1/Expense/Category/CreateRequest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Requests\API\V1\Expense\Category;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class CreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+        ];
+    }
+
+
+    /**
+     * Handles failed validation by formatting the validation errors and throwing a ValidationException.
+     *
+     * This method is called when validation fails in a form request. It uses the `error` method
+     * from the `ApiResponse` trait to generate a standardized Errorsresponse with the validation
+     * Errorsmessages and a 422 HTTP status code. It then throws a `ValidationException` with the
+     * formatted response.
+     *
+     * @param Validator $validator The validator instance containing the validation errors.
+     *
+     * @return void Throws a ValidationException with a formatted Errorsresponse.
+     *
+     * @throws ValidationException The exception is thrown to halt further processing and return validation errors.
+     */
+    protected function failedValidation(Validator $validator): never
+    {
+        $errors = $validator->errors()->getMessages();
+        $message = null;
+        $fields = [
+            'name'
+        ];
+
+        foreach ($fields as $field) {
+            if (isset($errors[$field])) {
+                $message = $errors[$field][0];
+                break;
+            }
+        }
+
+        $response = $this->error(
+            422,
+            $message,
+            $validator->errors(),
+        );
+        throw new ValidationException($validator, $response);
+    }
+}

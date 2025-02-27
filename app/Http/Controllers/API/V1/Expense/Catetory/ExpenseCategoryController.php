b13dda71904cedfba14b8865fd6425869b30a620
mdiktushar <md.tushar1804@gmail.com>
2025-02-27 12:30:29 +0600

    resource

--- app/Http/Controllers/API/V1/Expense/Catetory/ExpenseCategoryController.php
+++ app/Http/Controllers/API/V1/Expense/Catetory/ExpenseCategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API\V1\Expense\Catetory;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\V1\Expense\Category\CreateRequest;
+use App\Http\Resources\Api\V1\Expense\Category\StoreResource;
 use App\Services\API\V1\Expense\Catetory\ExpenseCategoryService;
 use App\Traits\V1\ApiResponse;
 use Exception;
@@ -50,7 +51,7 @@ class ExpenseCategoryController extends Controller
         try {
             $validatedData = $createRequest->validated();
             $response = $this->expenseCategoryService->create($validatedData);
-            return $this->success(201, 'Category Created Successfully', $response);
+            return $this->success(201, 'Category Created Successfully', new StoreResource($response));
         } catch (Exception $e) {
             Log::error('ExpenseCategoryController::index', ['error' => $e->getMessage()]);
             return $this->error(500, 'Server Error', $e->getMessage());

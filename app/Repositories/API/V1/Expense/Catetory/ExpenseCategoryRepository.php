b68a5c1d312638558fa358fdb8c064ceead0eeae
mdiktushar <md.tushar1804@gmail.com>
2025-02-27 11:33:56 +0600

    expenseCategory repo

--- app/Repositories/API/V1/Expense/Catetory/ExpenseCategoryRepository.php
+++ app/Repositories/API/V1/Expense/Catetory/ExpenseCategoryRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories\API\V1\Expense\Catetory;
 
+use App\Helpers\Helper;
 use App\Models\ExpenseCategory;
 use Exception;
 use Illuminate\Database\Eloquent\Collection;
@@ -13,13 +14,47 @@ class ExpenseCategoryRepository implements ExpenseCategoryRepositoryInterface
      * gets all the data form the expense category table
      * @return \Illuminate\Database\Eloquent\Collection<int, ExpenseCategory>
      */
-    public function getExpenseCategories():Collection
+    public function getExpenseCategories(): Collection
     {
         try {
             $categories = ExpenseCategory::select('id', 'name', 'slug')->get();
             return $categories;
-        }catch (Exception $e) {
+        } catch (Exception $e) {
             Log::error('ExpenseCategoryRepository::getExpenseCategories', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+
+    /**
+     * createExpenseCategory
+     * @param array $categories
+     * @return ExpenseCategory
+     */
+    public function createExpenseCategory(array $categories): ExpenseCategory
+    {
+        try {
+            return ExpenseCategory::create([
+                'name' => $categories['name'],
+                'slug' => Helper::generateUniqueSlug($categories['name'], 'expense_categories'),
+            ]);
+        } catch (Exception $e) {
+            Log::error('ExpenseCategoryRepository::createExpenseCategory', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+
+
+    /**
+     * searchByNameExpenseCategory
+     * @param string $name
+     * @return Collection<int, ExpenseCategory>
+     */
+    public function searchByNameExpenseCategory(string $name): Collection
+    {
+        try {
+            return ExpenseCategory::where('name', 'like', '%' . $name . '%')->get();
+        } catch (Exception $e) {
+            Log::error('ExpenseCategoryRepository::searchByNameExpenseCategory', ['error' => $e->getMessage()]);
             throw $e;
         }
     }
--- app/Repositories/API/V1/Expense/Catetory/ExpenseCategoryRepositoryInterface.php
+++ app/Repositories/API/V1/Expense/Catetory/ExpenseCategoryRepositoryInterface.php
@@ -11,5 +11,19 @@ interface ExpenseCategoryRepositoryInterface
      * gets all the data form the expense category table
      * @return \Illuminate\Database\Eloquent\Collection<int, ExpenseCategory>
      */
-    public function getExpenseCategories():Collection;
+    public function getExpenseCategories(): Collection;
+
+    /**
+     * createExpenseCategory
+     * @param array $categories
+     * @return ExpenseCategory
+     */
+    public function createExpenseCategory(array $categories): ExpenseCategory;
+
+    /**
+     * searchByNameExpenseCategory
+     * @param string $name
+     * @return Collection<int, ExpenseCategory>
+     */
+    public function searchByNameExpenseCategory(string $name): Collection;
 }

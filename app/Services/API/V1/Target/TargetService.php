df4fb319f0b43eebf924245dfe16782fb598418f
mdiktushar <md.tushar1804@gmail.com>
2025-03-08 10:15:14 +0600

    target service store

---
+++ app/Services/API/V1/Target/TargetService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services\API\V1\Target;
+
+use App\Repositories\API\V1\Target\TargetRepositoryInterface;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+class TargetService
+{
+    protected TargetRepositoryInterface $targetRepository;
+    protected $user;
+
+    /**
+     * construct
+     * @param \App\Repositories\API\V1\Target\TargetRepositoryInterface $targetRepository
+     */
+    public function __construct(TargetRepositoryInterface $targetRepository)
+    {
+        $this->user = Auth::use();
+        $this->targetRepository = $targetRepository;
+    }
+
+    /**
+     * store
+     * @param array $credentials
+     * @return \App\Models\Target
+     */
+    public function store(array $credentials)
+    {
+        try {
+            return $this->targetRepository->storeTarget($credentials, $this->user->id);
+        } catch (Exception $e) {
+            Log::error('App\Services\API\V1\Target\TargetService::store', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+}

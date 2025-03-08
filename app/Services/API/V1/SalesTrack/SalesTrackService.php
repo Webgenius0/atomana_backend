07f223e5e9113f1fe255ecc39e5922afd75bfbea
mdiktushar <md.tushar1804@gmail.com>
2025-03-08 14:20:15 +0600

    proper comment

--- app/Services/API/V1/SalesTrack/SalesTrackService.php
+++ app/Services/API/V1/SalesTrack/SalesTrackService.php
@@ -87,7 +87,7 @@ class SalesTrackService
      * @param string $filter
      * @return array{price: float, target: float}
      */
-    protected function adminCurrentStatus(string $filter): array
+    private function adminCurrentStatus(string $filter): array
     {
         try {
             $currentDate = Carbon::now();
@@ -111,8 +111,6 @@ class SalesTrackService
                 $currentAmount = $this->salesTrackRepository->businessCurrentYearColseSalesTrack($this->businessId, $yearStart, $yearEnd);
                 $target = $this->targetRepository->getRangeTarget($this->user->id, $yearStart, $yearEnd, 'current_sales');
             }
-            Log::info($currentAmount);
-            Log::info($target);
             if ($target) {
                 $percentage = ($currentAmount * 100) / $target;
             }
@@ -132,12 +130,13 @@ class SalesTrackService
      * @param string $filter
      * @return array{price: float, target: float}
      */
-    protected function agentCurrentStatus(string $filter): array
+    private function agentCurrentStatus(string $filter): array
     {
         try {
             $currentDate = Carbon::now();
             $currentAmount = null;
             $target = null;
+            $percentage = null;
             if ($filter == 'monthly') {
                 $startOfMonth = $this->getStartOfMonth($currentDate);
                 $endOfMonth = $currentDate->endOfMonth();
@@ -155,8 +154,9 @@ class SalesTrackService
                 $target = $this->targetRepository->getRangeTarget($this->user->id, $yearStart, $yearEnd, 'current_sales');
             }
             return [
-                'target' => (float) $target,
-                'price' => (float) $currentAmount,
+                'target' => number_format((float) $target, 2),
+                'target_fill' => number_format((float) $currentAmount, 2),
+                'percentage' => number_format((float) $percentage, 2),
             ];
         } catch (Exception $e) {
             Log::error('SalesTrackService::agentCurrentStatus', ['error' => $e->getMessage()]);
@@ -171,7 +171,12 @@ class SalesTrackService
      */
     private function getStartOfMonth(Carbon $date)
     {
-        return Carbon::create($date->year, $date->month, 1);
+        try {
+            return Carbon::create($date->year, $date->month, 1);
+        }catch(Exception $e) {
+            Log::error('SalesTrackService::getStartOfMonth', ['error' => $e->getMessage()]);
+            throw $e;
+        }
     }
 
     /**
@@ -181,7 +186,12 @@ class SalesTrackService
      */
     private function getCurrentYearStartDate(Carbon $date)
     {
-        return Carbon::create($date->year, 1, 1)->startOfDay(); // January 1st
+        try {
+            return Carbon::create($date->year, 1, 1)->startOfDay(); // January 1st
+        }catch(Exception $e) {
+            Log::error('SalesTrackService::getCurrentYearStartDate', ['error' => $e->getMessage()]);
+            throw $e;
+        }
     }
 
     /**
@@ -191,7 +201,12 @@ class SalesTrackService
      */
     private function getCurrentYearEndDate(Carbon $date)
     {
-        return Carbon::create($date->year, 12, 31)->endOfDay(); // December 31st
+        try {
+            return Carbon::create($date->year, 12, 31)->endOfDay(); // December 31st
+        }catch(Exception $e) {
+            Log::error('SalesTrackService::getCurrentYearEndDate', ['error' => $e->getMessage()]);
+            throw $e;
+        }
     }
 
     /**
@@ -201,16 +216,21 @@ class SalesTrackService
      */
     private function getCurrentQuarterStartDate(Carbon $date)
     {
-        $month = $date->month;
+        try {
+            $month = $date->month;
 
-        if ($month <= 3) {
-            return Carbon::create($date->year, 1, 1);  // Q1 starts on January 1st
-        } elseif ($month <= 6) {
-            return Carbon::create($date->year, 4, 1);  // Q2 starts on April 1st
-        } elseif ($month <= 9) {
-            return Carbon::create($date->year, 7, 1);  // Q3 starts on July 1st
-        } else {
-            return Carbon::create($date->year, 10, 1); // Q4 starts on October 1st
+            if ($month <= 3) {
+                return Carbon::create($date->year, 1, 1);  // Q1 starts on January 1st
+            } elseif ($month <= 6) {
+                return Carbon::create($date->year, 4, 1);  // Q2 starts on April 1st
+            } elseif ($month <= 9) {
+                return Carbon::create($date->year, 7, 1);  // Q3 starts on July 1st
+            } else {
+                return Carbon::create($date->year, 10, 1); // Q4 starts on October 1st
+            }
+        }catch(Exception $e) {
+            Log::error('SalesTrackService::getCurrentQuarterStartDate', ['error' => $e->getMessage()]);
+            throw $e;
         }
     }
 
@@ -221,16 +241,21 @@ class SalesTrackService
      */
     private function getCurrentQuarterEndDate(Carbon $date)
     {
-        $month = $date->month;
+        try {
+            $month = $date->month;
 
-        if ($month <= 3) {
-            return Carbon::create($date->year, 3, 31);  // Q1 ends on March 31st
-        } elseif ($month <= 6) {
-            return Carbon::create($date->year, 6, 30);  // Q2 ends on June 30th
-        } elseif ($month <= 9) {
-            return Carbon::create($date->year, 9, 30);  // Q3 ends on September 30th
-        } else {
-            return Carbon::create($date->year, 12, 31); // Q4 ends on December 31st
+            if ($month <= 3) {
+                return Carbon::create($date->year, 3, 31);  // Q1 ends on March 31st
+            } elseif ($month <= 6) {
+                return Carbon::create($date->year, 6, 30);  // Q2 ends on June 30th
+            } elseif ($month <= 9) {
+                return Carbon::create($date->year, 9, 30);  // Q3 ends on September 30th
+            } else {
+                return Carbon::create($date->year, 12, 31); // Q4 ends on December 31st
+            }
+        }catch(Exception $e) {
+            Log::error('SalesTrackService::getCurrentQuarterEndDate', ['error' => $e->getMessage()]);
+            throw $e;
         }
     }
 }

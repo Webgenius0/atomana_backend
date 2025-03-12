dedef27d34f896f7a096c07c494d0f46f98769e5
mdiktushar <md.tushar1804@gmail.com>
2025-03-12 10:05:55 +0600

    properrtysource service

--- app/Services/API/V1/Property/Source/PropertySourceService.php
+++ app/Services/API/V1/Property/Source/PropertySourceService.php
@@ -1,8 +1,37 @@
 <?php
-    
+
 namespace App\Services\API\V1\Property\Source;
+
+use App\Repositories\API\V1\Property\Source\PropertySourceRepositoryInterface;
+use Exception;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Log;
 
 class PropertySourceService
 {
-    // Your service logic goes here
+    protected PropertySourceRepositoryInterface $propertySourceRepository;
+
+    /**
+     * construct
+     * @param \App\Repositories\API\V1\Property\Source\PropertySourceRepositoryInterface $propertySourceRepository
+     */
+    public function __construct(PropertySourceRepositoryInterface $propertySourceRepository)
+    {
+        $this->propertySourceRepository = $propertySourceRepository;
+    }
+
+
+    /**
+     * getDropdown
+     * @return \Illuminate\Database\Eloquent\Collection<int, \App\Models\PropertySource>
+     */
+    public function getDropdown(): Collection
+    {
+        try {
+            return $this->propertySourceRepository->getSource();
+        }catch (Exception $e) {
+            Log::error('App\Services\API\V1\Property\Source\PropertySourceService::getDropdown', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
 }

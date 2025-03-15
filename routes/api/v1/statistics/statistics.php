3aac711736bc2624bd7ad6e107e0596d2bd4c9b8
mdiktushar <md.tushar1804@gmail.com>
2025-03-15 12:34:52 +0600

    getting agent data

--- routes/api/v1/statistics/statistics.php
+++ routes/api/v1/statistics/statistics.php
@@ -10,19 +10,22 @@ Route::prefix('v1/statistic')->name('statistic.')->middleware(['auth:api', 'veri
     Route::middleware(['authorized'])->controller(HomeStatisticController::class)->group(function () {
 
         Route::get('/current-sales/{filter}', 'currentSales')->name('current.sales')
-        ->where('filter', 'monthly|quarterly|yearly');
+            ->where('filter', 'monthly|quarterly|yearly');
 
         Route::get('/units-sold/{filter}', 'unitsSold')->name('units.sold')
-        ->where('filter', 'monthly|quarterly|yearly');
+            ->where('filter', 'monthly|quarterly|yearly');
 
         Route::get('/expenses/{filter}', 'expenses')->name('expenses')
-        ->where('filter', 'monthly|quarterly|yearly');
+            ->where('filter', 'monthly|quarterly|yearly');
 
         Route::get('/net-profit/{filter}', 'netProfit')->name('net.profit')
-        ->where('filter', 'monthly|quarterly|yearly');
+            ->where('filter', 'monthly|quarterly|yearly');
+
+        Route::get('/agent-data/{userId}/{filter}', 'agentData')->name('agent.data')
+            ->where('filter', 'monthly|quarterly|yearly');
 
         Route::get('/leaderboard/{sortedBy}/{filter}', 'leaderboard')->name('leaderboard')
-        ->where('sortedBy', 'highest_sales|highest_sold_volume')
-        ->where('filter', 'monthly|quarterly|yearly');
+            ->where('sortedBy', 'highest_sales|highest_sold_volume')
+            ->where('filter', 'monthly|quarterly|yearly');
     });
 });

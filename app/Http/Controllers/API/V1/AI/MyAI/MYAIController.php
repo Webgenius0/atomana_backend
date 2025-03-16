1e18043f2fbdc7512486a7be3f104469fe485a37
mdiktushar <md.tushar1804@gmail.com>
2025-03-16 12:04:10 +0600

    function define for myAI

--- app/Http/Controllers/API/V1/AI/MyAI/MYAIController.php
+++ app/Http/Controllers/API/V1/AI/MyAI/MYAIController.php
@@ -3,9 +3,52 @@
 namespace App\Http\Controllers\API\V1\AI\MyAI;
 
 use App\Http\Controllers\Controller;
+use App\Models\MyAI;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class MYAIController extends Controller
 {
-    //
+    public function index(): JsonResponse
+    {
+        try {
+            return $this->success(200, 'All Chats.');
+        } catch (Exception $e) {
+            Log::error('App\Http\Controllers\API\V1\AI\MyAIController::index', ['error' => $e->getMessage()]);
+            return $this->error(500, 'Server Error.', $e->getMessage());
+        }
+    }
+
+    public function store(): JsonResponse
+    {
+        try {
+            return $this->success(200, 'All Chats.');
+        } catch (Exception $e) {
+            Log::error('App\Http\Controllers\API\V1\AI\MyAIController::store', ['error' => $e->getMessage()]);
+            return $this->error(500, 'Server Error.', $e->getMessage());
+        }
+    }
+
+    public function show(MyAI $myAI): JsonResponse
+    {
+        try {
+            return $this->success(200, 'All Chats.');
+        } catch (Exception $e) {
+            Log::error('App\Http\Controllers\API\V1\AI\MyAIController::store', ['error' => $e->getMessage()]);
+            return $this->error(500, 'Server Error.', $e->getMessage());
+        }
+    }
+
+
+    public function chat(MyAI $myAI): JsonResponse
+    {
+        try {
+            return $this->success(200, 'All Chats.');
+        } catch (Exception $e) {
+            Log::error('App\Http\Controllers\API\V1\AI\MyAIController::store', ['error' => $e->getMessage()]);
+            return $this->error(500, 'Server Error.', $e->getMessage());
+        }
+    }
 }

ff620a5fd17f8da0271cca54ddd47037b7800a34
mdiktushar <md.tushar1804@gmail.com>
2025-03-17 14:51:13 +0600

    social request

---
+++ app/Http/Requests/API/V1/Profile/SocialMediaRequest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Requests\API\V1\Profile;
+
+use App\Traits\V1\ApiResponse;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class SocialMediaRequest extends FormRequest
+{
+    use ApiResponse;
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'facebook' => 'nullable|url',
+            'instagram' => 'nullable|url',
+            'twitter' => 'nullable|url',
+        ];
+    }
+
+        /**
+     * failedValidation
+     * @param \Illuminate\Contracts\Validation\Validator $validator
+     * @throws \Illuminate\Validation\ValidationException
+     * @return never
+     */
+    protected function failedValidation(Validator $validator): never
+    {
+        $errors = $validator->errors()->getMessages();
+        $message = null;
+        $fields = [
+            'facebook',
+            'instagram',
+            'twitter',
+        ];
+
+        foreach ($fields as $field) {
+            if (isset($errors[$field])) {
+                $message = $errors[$field][0];
+                break;
+            }
+        }
+
+        $response = $this->error(
+            422,
+            $message,
+            $validator->errors(),
+        );
+        throw new ValidationException($validator, $response);
+    }
+}

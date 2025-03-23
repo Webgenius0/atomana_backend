253d7762e58302941ced1216bfe3255714d6855e
mdiktushar <md.tushar1804@gmail.com>
2025-03-23 16:16:51 +0600

    get data function is declaired for the data fetching of agent earning

--- app/Services/API/V1/MyAgentEarning/MyAgentEarningService.php
+++ app/Services/API/V1/MyAgentEarning/MyAgentEarningService.php
@@ -4,11 +4,15 @@ namespace App\Services\API\V1\MyAgentEarning;
 
 use App\Repositories\API\V1\SalesTrack\SalesTrackRepositoryInterface;
 use App\Repositories\API\V1\UserYTC\UserYTCRepositoryInterface;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class MyAgentEarningService
 {
-    protected UserYTCRepositoryInterface $userYTCRepository;
-    protected SalesTrackRepositoryInterface $salesTrackRepository;
+    private UserYTCRepositoryInterface $userYTCRepository;
+    private SalesTrackRepositoryInterface $salesTrackRepository;
+    private $business;
 
     /**
      * construct
@@ -19,8 +23,17 @@ class MyAgentEarningService
     {
         $this->userYTCRepository = $userYTCRepository;
         $this->salesTrackRepository = $salesTrackRepository;
+        $this->business = Auth::user()->business();
     }
 
-
+    public function getData()
+    {
+        try {
+            
+        }catch (Exception $e){
+            Log::error("App\Services\API\V1\MyAgentEarning\MyAgentEarningService::getData". ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
 
 }

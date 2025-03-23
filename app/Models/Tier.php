345f803acef513279c9a04f7da53fc68dac945f9
mdiktushar <md.tushar1804@gmail.com>
2025-03-23 10:13:54 +0600

    model setup

--- app/Models/Tier.php
+++ app/Models/Tier.php
@@ -6,5 +6,39 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tier extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $guarded = [];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'id'          => 'integer',
+            'business_id' => 'integer',
+            'from'        => 'float',
+            'to'          => 'float',
+            'cut'         => 'float',
+            'diduct'      => 'float',
+            'created_at'  => 'datetime',
+            'updated_at'  => 'datetime',
+        ];
+    }
 }

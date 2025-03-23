014997b56f2f1910e9c365024895123a3d316c84
mdiktushar <md.tushar1804@gmail.com>
2025-03-23 16:12:20 +0600

    the controller is define for the MyAtentEarningService

--- app/Services/API/V1/MyAgentEarning/MyAgentEarningService.php
+++ app/Services/API/V1/MyAgentEarning/MyAgentEarningService.php
@@ -2,7 +2,25 @@
 
 namespace App\Services\API\V1\MyAgentEarning;
 
+use App\Repositories\API\V1\SalesTrack\SalesTrackRepositoryInterface;
+use App\Repositories\API\V1\UserYTC\UserYTCRepositoryInterface;
+
 class MyAgentEarningService
 {
-    // Your service logic goes here
+    protected UserYTCRepositoryInterface $userYTCRepository;
+    protected SalesTrackRepositoryInterface $salesTrackRepository;
+
+    /**
+     * construct
+     * @param \App\Repositories\API\V1\UserYTC\UserYTCRepositoryInterface $userYTCRepository
+     * @param \App\Repositories\API\V1\SalesTrack\SalesTrackRepositoryInterface $salesTrackRepository
+     */
+    public function __construct(UserYTCRepositoryInterface $userYTCRepository, SalesTrackRepositoryInterface $salesTrackRepository)
+    {
+        $this->userYTCRepository = $userYTCRepository;
+        $this->salesTrackRepository = $salesTrackRepository;
+    }
+
+
+
 }

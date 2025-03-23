97c41d47776cbdb48393658cadded8210a3bf029
mdiktushar <md.tushar1804@gmail.com>
2025-03-23 16:08:56 +0600

    Function is created for the userYtc

--- app/Repositories/API/V1/UserYTC/UserYTCRepository.php
+++ app/Repositories/API/V1/UserYTC/UserYTCRepository.php
@@ -1,8 +1,25 @@
 <?php
-        
+
 namespace App\Repositories\API\V1\UserYTC;
+
+use App\Models\UserYTCView;
+use Exception;
+use Illuminate\Support\Facades\Log;
 
 class UserYTCRepository implements UserYTCRepositoryInterface
 {
-    // Your Repository logic goes here
+    /**
+     * userYtc of the user.
+     * @param int $userId
+     * @return UserYTCView|null
+     */
+    public function userYtc(int $userId): UserYTCView|null
+    {
+        try {
+            return UserYTCView::where("user_id", $userId)->first();
+        } catch (Exception $e) {
+            Log::error('App\Repositories\API\V1\UserYTC\UserYTCRepository::userYtc', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
 }
--- app/Repositories/API/V1/UserYTC/UserYTCRepositoryInterface.php
+++ app/Repositories/API/V1/UserYTC/UserYTCRepositoryInterface.php
@@ -1,8 +1,15 @@
 <?php
-    
+
 namespace App\Repositories\API\V1\UserYTC;
+
+use App\Models\UserYTCView;
 
 interface UserYTCRepositoryInterface
 {
-    // Define the methods your repository should implement
+    /**
+     * userYtc of the user.
+     * @param int $userId
+     * @return \App\Models\UserYTCView|null
+     */
+    public function userYtc(int $userId): UserYTCView|null;
 }

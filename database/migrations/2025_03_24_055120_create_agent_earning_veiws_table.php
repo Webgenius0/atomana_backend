738fa355913f0118c899b6ca6182e90af1acf844
mdiktushar <md.tushar1804@gmail.com>
2025-03-24 14:02:16 +0600

    agent earning table complete

--- database/migrations/2025_03_24_055120_create_agent_earning_veiws_table.php
+++ database/migrations/2025_03_24_055120_create_agent_earning_veiws_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
         DB::statement('DROP VIEW IF EXISTS agent_earning_views');
         DB::statement("
             CREATE VIEW agent_earning_views AS
-            SELECT
+                SELECT
                 st.user_id,
                 st.business_id,
                 COUNT(st.id) AS sales_closed,
@@ -22,7 +22,10 @@ return new class extends Migration
                 COALESCE(
                     ROUND(
                         (SUM(st.purchase_price) * 100) /
-                        (SELECT SUM(purchase_price) FROM sales_tracks WHERE status = 'close' AND closing_date >= ytc.current_year_start),
+                        (SELECT SUM(purchase_price)
+                        FROM sales_tracks
+                        WHERE status = 'close'
+                        AND closing_date >= (SELECT MIN(current_year_start) FROM user_y_t_c_views)),
                         2
                     ),
                     0
@@ -32,15 +35,23 @@ return new class extends Migration
                 SUM(sev.net_commission) AS net_commission_ytd,
                 SUM(sev.agent_net_income) AS agent_net_income_ytd,
                 SUM(sev.group_gross_income) AS group_gross_income_ytd,
-                SUM(sev.group_gross_income) AS group_net_ytd
-
+                SUM(sev.group_gross_income) AS group_net_ytd,
+                ROUND( COALESCE(
+                    (SUM(sev.group_gross_income) /
+                    NULLIF((SELECT SUM(group_gross_income)
+                            FROM sales_earning_view
+                            WHERE closing_date >= (SELECT MIN(current_year_start) FROM user_y_t_c_views)), 0)),
+                    0
+                ) * 100, 2) AS percentage_group_gross_income_ytd
             FROM sales_tracks st
             JOIN user_y_t_c_views ytc ON st.user_id = ytc.user_id
             JOIN business_info_views bi ON st.business_id = bi.business_id
-            JOIN sales_earning_view sev ON st.user_id = sev.user_id AND sev.closing_date >= ytc.current_year_start
+            JOIN sales_earning_view sev ON st.user_id = sev.user_id
+                AND sev.closing_date >= ytc.current_year_start
             WHERE st.status = 'close'
                 AND st.closing_date >= ytc.current_year_start
             GROUP BY st.user_id, st.business_id, ytc.current_year_start;
+
         ");
     }
 

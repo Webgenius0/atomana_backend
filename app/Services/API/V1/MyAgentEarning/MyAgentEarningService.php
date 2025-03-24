3bcb4569aa69631727fe8ff09781796486452f51
mdiktushar <md.tushar1804@gmail.com>
2025-03-24 14:52:02 +0600

    businessAgentEarning

--- app/Services/API/V1/MyAgentEarning/MyAgentEarningService.php
+++ app/Services/API/V1/MyAgentEarning/MyAgentEarningService.php
@@ -2,27 +2,39 @@
 
 namespace App\Services\API\V1\MyAgentEarning;
 
+use App\Repositories\API\V1\AgentEarning\AgentEarningRepositoryInterface;
 use App\Repositories\API\V1\SalesTrack\SalesTrackRepositoryInterface;
 use App\Repositories\API\V1\UserYTC\UserYTCRepositoryInterface;
 use Exception;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
 class MyAgentEarningService
 {
-    private $business;
+    private $businessId;
+    private AgentEarningRepositoryInterface $agentEarningRepository;
 
-
-    public function __construct()
+    /**
+     * construct
+     * @param \App\Repositories\API\V1\AgentEarning\AgentEarningRepositoryInterface $agentEarningRepository
+     */
+    public function __construct(AgentEarningRepositoryInterface $agentEarningRepository)
     {
-
-        $this->business = Auth::user()->business();
+        $this->$agentEarningRepository = $agentEarningRepository;
+        $this->businessId = Auth::user()->business();
     }
 
-    public function getData()
+
+    /**
+     * businessAgentEarning
+     * @return \Illuminate\Pagination\LengthAwarePaginator
+     */
+    public function businessAgentEarning(): LengthAwarePaginator
     {
         try {
-
+            $perPage = request()->query('per_page', 25);
+            return $this->agentEarningRepository->getAgentsOfBusiness($this->businessId, $perPage);
         }catch (Exception $e){
             Log::error("App\Services\API\V1\MyAgentEarning\MyAgentEarningService::getData". ['error' => $e->getMessage()]);
             throw $e;

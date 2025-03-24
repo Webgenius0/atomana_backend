d87fe268394b0facd47b957669952bd5d6fa4e21
mdiktushar <md.tushar1804@gmail.com>
2025-03-24 13:44:05 +0600

    view query update

--- database/migrations/2025_03_24_055113_create_sales_split_views_table.php
+++ database/migrations/2025_03_24_055113_create_sales_split_views_table.php
@@ -30,69 +30,117 @@ return new class extends Migration
                 -- Determine deduct_percentage based on override_split or tiers table
                 ROUND(
                     CASE
-                        WHEN st.override_split IS NOT NULL THEN st.override_split
-                        ELSE (
-                            SELECT t.deduct
-                            FROM tiers t
-                            WHERE t.business_id = st.business_id
-                            AND (
-                                (st.purchase_price >= t.from AND st.purchase_price <= t.to)
-                                OR (t.to IS NULL AND st.purchase_price >= t.from)
-                            )
-                            LIMIT 1
-                        )
-                    END, 2
-                ) AS deduct_percentage,
-
-                -- Calculate getting after overhead and round to 2 decimals
-                ROUND(
-                    CASE
-                        WHEN st.override_split IS NOT NULL THEN
-                            (st.commission_on_sale * st.purchase_price) / 100 - (st.override_split * (st.commission_on_sale * st.purchase_price) / 100) / 100
-                        ELSE (
-                            SELECT (st.commission_on_sale * st.purchase_price) / 100 - (t.deduct * (st.commission_on_sale * st.purchase_price) / 100) / 100
-                            FROM tiers t
-                            WHERE t.business_id = st.business_id
-                            AND (
-                                (st.purchase_price >= t.from AND st.purchase_price <= t.to)
-                                OR (t.to IS NULL AND st.purchase_price >= t.from)
-                            )
-                            LIMIT 1
-                        )
-                    END, 2
-                ) AS net_income,
-
-                -- Calculate agent split and round to 2 decimals
-                ROUND(
-                    CASE
-                        WHEN st.override_split IS NOT NULL THEN
-                            (((st.commission_on_sale * st.purchase_price) / 100 - (st.override_split * (st.commission_on_sale * st.purchase_price) / 100) / 100) * 100) / st.purchase_price
-                        ELSE
-                            100 * (
-                                SELECT (st.commission_on_sale * st.purchase_price) / 100 - (t.deduct * (st.commission_on_sale * st.purchase_price) / 100) / 100
-                                FROM tiers t
-                                WHERE t.business_id = st.business_id
-                                AND (
-                                    (st.purchase_price >= t.from AND st.purchase_price <= t.to)
-                                    OR (t.to IS NULL AND st.purchase_price >= t.from)
-                                )
-                                LIMIT 1
-                            ) / st.purchase_price
-                    END, 2
-                ) AS user_commision_split,
-
-                -- Show the tier range (from and to) if override_split is NULL
-                CASE
-                    WHEN st.override_split IS NOT NULL THEN 'Override Split'
-                    ELSE CONCAT(
-                        (SELECT CONCAT('From: ', t.from, ' To: ', t.to, ' deduct: ', t.deduct)
+                    WHEN st.override_split IS NOT NULL THEN st.override_split
+                    ELSE (
+                        SELECT t.deduct
                         FROM tiers t
                         WHERE t.business_id = st.business_id
                         AND (
                             (st.purchase_price >= t.from AND st.purchase_price <= t.to)
                             OR (t.to IS NULL AND st.purchase_price >= t.from)
                         )
-                        LIMIT 1)
+                        LIMIT 1
+                    )
+                    END, 2
+                ) AS deduct_percentage,
+
+                -- Calculate getting after overhead and round to 2 decimals
+                ROUND(
+                    CASE
+                    WHEN st.override_split IS NOT NULL THEN
+                    (st.commission_on_sale * st.purchase_price) / 100 - (st.override_split * (st.commission_on_sale * st.purchase_price) / 100) / 100
+                    ELSE (
+                        SELECT (st.commission_on_sale * st.purchase_price) / 100 - (t.deduct * (st.commission_on_sale * st.purchase_price) / 100) / 100
+                        FROM tiers t
+                        WHERE t.business_id = st.business_id
+                        AND (
+                            (st.purchase_price >= t.from AND st.purchase_price <= t.to)
+                            OR (t.to IS NULL AND st.purchase_price >= t.from)
+                        )
+                        LIMIT 1
+                    )
+                    END, 2
+                ) AS net_income,
+
+                -- Calculate agent split and round to 2 decimals
+                ROUND(
+                    CASE
+                    WHEN st.override_split IS NOT NULL THEN
+                    (((st.commission_on_sale * st.purchase_price) / 100 - (st.override_split * (st.commission_on_sale * st.purchase_price) / 100) / 100) * 100) / st.purchase_price
+                    ELSE
+                    100 * (
+                        SELECT (st.commission_on_sale * st.purchase_price) / 100 - (t.deduct * (st.commission_on_sale * st.purchase_price) / 100) / 100
+                        FROM tiers t
+                        WHERE t.business_id = st.business_id
+                        AND (
+                            (st.purchase_price >= t.from AND st.purchase_price <= t.to)
+                            OR (t.to IS NULL AND st.purchase_price >= t.from)
+                        )
+                        LIMIT 1
+                    ) / st.purchase_price
+                    END, 2
+                ) AS user_commision_split,
+
+
+                ROUND(st.purchase_price * st.commission_on_sale, 2) AS gross_commission_income,
+
+
+                ROUND(st.purchase_price * st.commission_on_sale * 0.10, 2) AS brokerage_cut,
+
+                ROUND((st.purchase_price * st.commission_on_sale) - (st.purchase_price * st.commission_on_sale * 0.10), 2) AS net_commission,
+
+
+                ROUND(ROUND(((st.purchase_price * st.commission_on_sale) - (st.purchase_price * st.commission_on_sale * 0.10)), 2) * ROUND(
+                    CASE
+                    WHEN st.override_split IS NOT NULL THEN
+                    (((st.commission_on_sale * st.purchase_price) / 100 - (st.override_split * (st.commission_on_sale * st.purchase_price) / 100) / 100) * 100) / st.purchase_price
+                    ELSE
+                    100 * (
+                        SELECT (st.commission_on_sale * st.purchase_price) / 100 - (t.deduct * (st.commission_on_sale * st.purchase_price) / 100) / 100
+                        FROM tiers t
+                        WHERE t.business_id = st.business_id
+                        AND (
+                            (st.purchase_price >= t.from AND st.purchase_price <= t.to)
+                            OR (t.to IS NULL AND st.purchase_price >= t.from)
+                        )
+                        LIMIT 1
+                    ) / st.purchase_price
+                    END, 2
+                ) / 100, 2) AS agent_net_income,
+
+
+                ROUND((st.purchase_price * st.commission_on_sale) - (st.purchase_price * st.commission_on_sale * 0.10) -
+                (((st.purchase_price * st.commission_on_sale) - (st.purchase_price * st.commission_on_sale * 0.10)) * ROUND(
+                    CASE
+                    WHEN st.override_split IS NOT NULL THEN
+                    (((st.commission_on_sale * st.purchase_price) / 100 - (st.override_split * (st.commission_on_sale * st.purchase_price) / 100) / 100) * 100) / st.purchase_price
+                    ELSE
+                    100 * (
+                        SELECT (st.commission_on_sale * st.purchase_price) / 100 - (t.deduct * (st.commission_on_sale * st.purchase_price) / 100) / 100
+                        FROM tiers t
+                        WHERE t.business_id = st.business_id
+                        AND (
+                            (st.purchase_price >= t.from AND st.purchase_price <= t.to)
+                            OR (t.to IS NULL AND st.purchase_price >= t.from)
+                        )
+                        LIMIT 1
+                    ) / st.purchase_price
+                    END, 2
+                ) / 100), 2) AS group_gross_income,
+
+
+                -- Show the tier range (from and to) if override_split is NULL
+                CASE
+                    WHEN st.override_split IS NOT NULL THEN 'Override Split'
+                    ELSE CONCAT(
+                        (SELECT CONCAT('From: ', t.from, ' To: ', t.to, ' deduct: ', t.deduct)
+                            FROM tiers t
+                            WHERE t.business_id = st.business_id
+                            AND (
+                                (st.purchase_price >= t.from AND st.purchase_price <= t.to)
+                                OR (t.to IS NULL AND st.purchase_price >= t.from)
+                            )
+                            LIMIT 1)
                     )
                 END AS tier_range
 
@@ -100,7 +148,6 @@ return new class extends Migration
                 sales_tracks st
             WHERE
                 st.status = 'close';
-
         ");
     }
 
--- database/migrations/2025_03_24_055120_create_agent_earning_veiws_table.php
+++ database/migrations/2025_03_24_055120_create_agent_earning_veiws_table.php
@@ -27,17 +27,17 @@ return new class extends Migration
                     ),
                     0
                 ) AS percentage_total_dollars_on_close_deal,
-                ROUND(SUM(st.purchase_price * st.commission_on_sale / 100), 2) AS gross_commission_income_ytd,
-                ROUND(SUM((st.purchase_price * st.commission_on_sale / 100) * 0.10) , 2) AS brokerage_cur_ytd,
-                ROUND(
-                    SUM(st.purchase_price * st.commission_on_sale / 100) -
-                    SUM((st.purchase_price * st.commission_on_sale / 100) * 0.10),
-                    2
-                ) AS net_commission_ytd
+                SUM(sev.gross_commission_income) AS gross_commission_income_ytd,
+                SUM(sev.brokerage_cut) AS brokerage_cut_ytd,
+                SUM(sev.net_commission) AS net_commission_ytd,
+                SUM(sev.agent_net_income) AS agent_net_income_ytd,
+                SUM(sev.group_gross_income) AS group_gross_income_ytd,
+                SUM(sev.group_gross_income) AS group_net_ytd
 
             FROM sales_tracks st
             JOIN user_y_t_c_views ytc ON st.user_id = ytc.user_id
             JOIN business_info_views bi ON st.business_id = bi.business_id
+            JOIN sales_earning_view sev ON st.user_id = sev.user_id AND sev.closing_date >= ytc.current_year_start
             WHERE st.status = 'close'
                 AND st.closing_date >= ytc.current_year_start
             GROUP BY st.user_id, st.business_id, ytc.current_year_start;

4e24c3278aded18f2b562cdbbb8bd2bd176e5e19
mdiktushar <md.tushar1804@gmail.com>
2025-03-26 14:16:18 +0600

    resource created

---
+++ app/Http/Resources/API/V1/Admin/AgentProfileShowResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources\API\V1\Admin;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AgentProfileShowResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return parent::toArray($request);
+    }
+}

c01d3f0eb01d25b5ebaa9a8d416fb0d95316d241
mdiktushar <md.tushar1804@gmail.com>
2025-03-26 14:45:48 +0600

    request created

---
+++ app/Http/Requests/API/V1/Admin/AgentProfileUpdateRequest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Requests\API\V1\Admin;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class AgentProfileUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $userId = $this->route('user')?->id;
+        return [
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'email' => 'required|email|unique:users,email,' . $userId,
+            "phone" => [
+                "required", // Ensures phone is provided
+                "regex:/^\+1\(\d{3}\)\d{3}-\d{4}$/", // Format: +1 (XXX) XXX-XXXX
+            ],
+            'contract_year_start' => 'required|date',
+            'total_commission_this_contract_year' => 'required',
+            'aggrement' => 'url',
+            'file' => 'url',
+        ];
+    }
+
+
+    /**
+     * failedValidation
+     * @param \Illuminate\Contracts\Validation\Validator $validator
+     * @throws \Illuminate\Validation\ValidationException
+     * @return never
+     */
+    protected function failedValidation(Validator $validator): never
+    {
+        $errors = $validator->errors()->getMessages();
+        $message = null;
+        $fields = [
+            'message',
+        ];
+
+        foreach ($fields as $field) {
+            if (isset($errors[$field])) {
+                $message = $errors[$field][0];
+                break;
+            }
+        }
+
+        $response = $this->error(
+            422,
+            $message,
+            $validator->errors(),
+        );
+        throw new ValidationException($validator, $response);
+    }
+}

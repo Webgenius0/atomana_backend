66f40760534abaae5084373e118b0c49a73c3ebf
mdiktushar <md.tushar1804@gmail.com>
2025-03-26 14:24:33 +0600

    resource set

--- app/Http/Resources/API/V1/Admin/AgentProfileShowResource.php
+++ app/Http/Resources/API/V1/Admin/AgentProfileShowResource.php
@@ -14,6 +14,17 @@ class AgentProfileShowResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        $data =  parent::toArray($request);
+        return [
+            "id"=> $data['id'],
+            "first_name"=> $data['first_name'],
+            "last_name"=> $data['last_name'],
+            "email"=> $data['email'],
+            "phone"=> $data['profile']['phone'],
+            "contract_year_start"=> $data['profile']['contract_year_start'],
+            "total_commission_this_contract_year"=> $data['profile']['total_commission_this_contract_year'],
+            "aggrement"=> $data['profile']['aggrement'],
+            "file"=> $data['profile']['file'],
+        ];
     }
 }

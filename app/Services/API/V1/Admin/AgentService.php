9102742a0acb3a45dc35f42cfdc6016c6098f691
mdiktushar <md.tushar1804@gmail.com>
2025-03-26 13:56:05 +0600

    agent service update

--- app/Services/API/V1/Admin/AgentService.php
+++ app/Services/API/V1/Admin/AgentService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\API\V1\Admin;
 
+use App\Models\User;
 use App\Repositories\API\V1\Admin\AgentRepositoryInterface;
 use Exception;
 use Illuminate\Support\Facades\Auth;
@@ -28,6 +29,37 @@ class AgentService
             return $data;
         }catch(Exception $e) {
             Log::error('App\Services\API\V1\Admin\AgentService::getAgents', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+
+
+    /**
+     * getAgentDetails
+     * @param \App\Models\User $user
+     */
+    public function getAgentDetails(User $user)
+    {
+        try {
+            return $this->agentRepository->getAgentProfileById($user->id);
+        }catch(Exception $e) {
+            Log::error('App\Services\API\V1\Admin\AgentService::getAgentDetails', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+
+    /**
+     * updateAgentProfile
+     * @param \App\Models\User $user
+     * @param array $credentials
+     * @return void
+     */
+    public function updateAgentProfile(User $user, array $credentials)
+    {
+        try {
+            $this->agentRepository->updateAgentProfileById($credentials, $user->id);
+        }catch(Exception $e) {
+            Log::error('App\Services\API\V1\Admin\AgentService::updateAgentProfile', ['error' => $e->getMessage()]);
             throw $e;
         }
     }

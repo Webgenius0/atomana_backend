7078d1f7d7f526a9b444ee9c5d648d51908c51f7
mdiktushar <md.tushar1804@gmail.com>
2025-04-05 15:34:54 +0600

    openhouse feedback created

---
+++ app/Repositories/API/V1/OpenHouse/FeedbackRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Repositories\API\V1\OpenHouse;
+
+use App\Models\OpenHouseFeedback;
+use Exception;
+
+class FeedbackRepository implements FeedbackRepositoryInterface
+{
+    /**
+     * createOpenHouseFeedback
+     * @param array $data
+     * @param int $userId
+     * @param int $businessId
+     * @param int $propertyId
+     * @return OpenHouseFeedback
+     */
+    public function createOpenHouseFeedback(array $data, int $userId, int $businessId, int $propertyId): OpenHouseFeedback
+    {
+        try {
+            return OpenHouseFeedback::create([
+                'user_id'             => $userId,
+                'business_id'         => $businessId,
+                'property_id'         => $propertyId,
+                'open_house_id'       => $data['open_house_id'],
+                'people_count'        => $data['people_count'],
+                'feedback'            => $data['feedback'],
+                'additional_feedback' => $data['additional_feedback'],
+            ]);
+        }catch (Exception $e) {
+            throw $e;
+        }
+    }
+}
---
+++ app/Repositories/API/V1/OpenHouse/FeedbackRepositoryInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repositories\API\V1\OpenHouse;
+
+use App\Models\OpenHouseFeedback;
+
+interface FeedbackRepositoryInterface
+{
+    /**
+     * createOpenHouseFeedback
+     * @param array $data
+     * @param int $userId
+     * @param int $businessId
+     * @param int $propertyId
+     * @return OpenHouseFeedback
+     */
+    public function createOpenHouseFeedback(array $data, int $userId, int $businessId, int $propertyId): OpenHouseFeedback;
+}

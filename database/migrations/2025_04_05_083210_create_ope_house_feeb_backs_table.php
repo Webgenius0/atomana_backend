62f703fb8f21631fb7fbbf9f4e3497a9d93227fb
mdiktushar <md.tushar1804@gmail.com>
2025-04-05 14:35:46 +0600

    house_feeb_backs_table creaed

---
+++ database/migrations/2025_04_05_083210_create_ope_house_feeb_backs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ope_house_feeb_backs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('business_id')->constrained('businesses')->cascadeOnDelete();
+            $table->foreignId('property_id')->constrained('properties')->cascadeOnDelete();
+            $table->foreignId('open_house_id')->constrained('open_houses')->cascadeOnDelete();
+
+            $table->integer('people_count')->default(0);
+            $table->longText('feedback');
+            $table->longText('additional_feedback');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ope_house_feeb_backs');
+    }
+};

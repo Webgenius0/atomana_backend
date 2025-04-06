9a7ac17e1ea861e12d8345b5efe67076dfccec4c
mdiktushar <md.tushar1804@gmail.com>
2025-04-06 12:28:40 +0600

    database update function

--- app/Repositories/API/V1/Expense/ExpenseRepository.php
+++ app/Repositories/API/V1/Expense/ExpenseRepository.php
@@ -114,4 +114,159 @@ class ExpenseRepository implements ExpenseRepositoryInterface
             throw $e;
         }
     }
+
+    /**
+     * updateCategory
+     * @param int $id
+     * @param int $categoryId
+     * @return bool
+     */
+    public function updateCategory(int $id, int $categoryId) {
+        try {
+            return Expense::findOrFail($id)->update([
+                'expense_category_id' => $categoryId,
+            ]);
+        } catch (Exception $e) {
+            Log::error('ExpenseRepository::updateCategory', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+
+    /**
+     * updateSubCategory
+     * @param int $id
+     * @param int $subCategoryId
+     * @return bool
+     */
+    public function updateSubCategory(int $id, int $subCategoryId) {
+        try {
+            return Expense::findOrFail($id)->update([
+                'expense_sub_category_id' => $subCategoryId,
+            ]);
+        } catch (Exception $e) {
+            Log::error('ExpenseRepository::updateSubCategory', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+
+    /**
+     * updateDescription
+     * @param int $id
+     * @param string $description
+     * @return bool
+     */
+    public function updateDescription(int $id, string $description) {
+        try {
+            return Expense::findOrFail($id)->update([
+                'description' => $description,
+            ]);
+        } catch (Exception $e) {
+            Log::error('ExpenseRepository::updateDescription', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+
+    /**
+     * updateAmount
+     * @param int $id
+     * @param float $amount
+     * @return bool
+     */
+    public function updateAmount(int $id, float $amount) {
+        try {
+            return Expense::findOrFail($id)->update([
+                'amount' => $amount,
+            ]);
+        } catch (Exception $e) {
+            Log::error('ExpenseRepository::updateAmount', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+
+    /**
+     * paymentMethodAmount
+     * @param int $id
+     * @param int $paymentMethodId
+     * @return bool
+     */
+    public function updatePaymentMethodAmount(int $id, int $paymentMethodId) {
+        try {
+            return Expense::findOrFail($id)->update([
+                'payment_method_id' => $paymentMethodId,
+            ]);
+        } catch (Exception $e) {
+            Log::error('ExpenseRepository::updatePaymentMethodAmount', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+
+    /**
+     * updatePayee
+     * @param int $id
+     * @param string $payee
+     * @return bool
+     */
+    public function updatePayee(int $id, string $payee) {
+        try {
+            return Expense::findOrFail($id)->update([
+                'payee' => $payee,
+            ]);
+        } catch (Exception $e) {
+            Log::error('ExpenseRepository::updatePayee', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+
+    /**
+     * updateReimbursable
+     * @param int $id
+     * @return bool
+     */
+    public function updateReimbursable(int $id)
+    {
+        try {
+            $expense = Expense::findOrFail($id);
+            $expense->reimbursable = !$expense->reimbursable;
+            $expense->save();
+            return true;
+        } catch (Exception $e) {
+            Log::error('ExpenseRepository::updateReimbursable', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+
+    /**
+     * updateListing
+     * @param int $id
+     * @param string $listing
+     * @return bool
+     */
+    public function updateListing(int $id, string $listing) {
+        try {
+            return Expense::findOrFail($id)->update([
+                'listing' => $listing,
+            ]);
+        } catch (Exception $e) {
+            Log::error('ExpenseRepository::updateListing', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+
+    /**
+     * updateNote
+     * @param int $id
+     * @param string $note
+     * @return bool
+     */
+    public function updateNote(int $id, string $note) {
+        try {
+            return Expense::findOrFail($id)->update([
+                'note' => $note,
+            ]);
+        } catch (Exception $e) {
+            Log::error('ExpenseRepository::updateNote', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+
 }

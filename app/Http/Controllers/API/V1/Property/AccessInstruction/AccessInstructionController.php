0d5d40d43b31eedddbc833028099f7fc260c0b61
mdiktushar <md.tushar1804@gmail.com>
2025-04-10 14:21:47 +0600

    property access instruction controller created

---
+++ app/Http/Controllers/API/V1/Property/AccessInstruction/AccessInstructionController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\API\V1\Property\AccessInstruction;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\API\V1\Property\AccessInstruction\CreateRequest;
+use App\Models\PropertyAccessInstruction;
+use App\Services\API\V1\Property\AccessInstruction\AccessInstructionService;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class AccessInstructionController extends Controller
+{
+    private AccessInstructionService $accessInstructionService;
+
+    /**
+     * construct
+     * @param \App\Services\API\V1\Property\AccessInstruction\AccessInstructionService $accessInstructionService
+     */
+    public function __construct(AccessInstructionService $accessInstructionService)
+    {
+        $this->accessInstructionService = $accessInstructionService;
+    }
+
+    /**
+     * index
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index():JsonResponse
+    {
+        try {
+            $response = $this->accessInstructionService->getIndex();
+            return $this->success(200, 'List of Property Access Instruciton', $response);
+        }catch (Exception $e){
+            Log::error('AccessInstructionController::index', ['error' => $e->getMessage()]);
+            return $this->error(500, 'Server Error', $e->getMessage());
+        }
+    }
+
+    /**
+     *store
+     * @param \App\Http\Requests\API\V1\Property\AccessInstruction\CreateRequest $createRequest
+     * @return JsonResponse
+     */
+    public function store(CreateRequest $createRequest):JsonResponse
+    {
+        try {
+            $validatedData = $createRequest->validated();
+            $response = $this->accessInstructionService->createAccessInstruction($validatedData);
+            return $this->success(200, 'Created Successfully.', $response);
+        }catch (Exception $e){
+            Log::error('AccessInstructionController::store', ['error' => $e->getMessage()]);
+            return $this->error(500, 'Server Error.', $e->getMessage());
+        }
+    }
+
+    /**
+     * show
+     * @param \App\Models\PropertyAccessInstruction $propertyAccessInstruction
+     * @return JsonResponse
+     */
+    public function show(PropertyAccessInstruction $propertyAccessInstruction):JsonResponse
+    {
+        try {
+            $response = $this->accessInstructionService->showSingleAccessInstruction($propertyAccessInstruction->id);
+            return $this->success(200, 'Created Successfully.', $response);
+        }catch (Exception $e){
+            Log::error('AccessInstructionController::store', ['error' => $e->getMessage()]);
+            return $this->error(500, 'Server Error.', $e->getMessage());
+        }
+    }
+}
---
+++ app/Http/Requests/API/V1/Property/AccessInstruction/CreateRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\API\V1\Property\AccessInstruction;
+
+use App\Traits\V1\ApiResponse;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateRequest extends FormRequest
+{
+    use ApiResponse;
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}

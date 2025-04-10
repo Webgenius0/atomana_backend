a0b28a7a6d0a106cdb7b44281631dc11e372833a
mdiktushar <md.tushar1804@gmail.com>
2025-04-10 14:17:43 +0600

    update

--- app/Http/Controllers/API/V1/Property/Type/PropertyTypeController.php
+++ app/Http/Controllers/API/V1/Property/Type/PropertyTypeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API\V1\Property\Type;
 use App\Http\Controllers\Controller;
 use App\Services\API\V1\Property\Type\PropertyTypeService;
 use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -25,14 +26,15 @@ class PropertyTypeController extends Controller
      * index
      * @return \Illuminate\Http\JsonResponse
      */
-    public function index()
+    public function index(): JsonResponse
     {
         try {
             $response = $this->propertyTypeService->getDropdown();
             return $this->success(200, 'Property Index', $response);
-        }catch (Exception $e) {
+        } catch (Exception $e) {
             Log::error('PropertyTypeController::index', ['error' => $e->getMessage()]);
             return $this->error(500, 'Server Error', $e->getMessage());
         }
     }
+    
 }

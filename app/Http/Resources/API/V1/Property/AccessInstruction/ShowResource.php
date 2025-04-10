31d85ecaf1169e4630d080de0fb1f711c142ac02
mdiktushar <md.tushar1804@gmail.com>
2025-04-10 16:20:13 +0600

    null handeling

--- app/Http/Resources/API/V1/Property/AccessInstruction/ShowResource.php
+++ app/Http/Resources/API/V1/Property/AccessInstruction/ShowResource.php
@@ -16,30 +16,34 @@ class ShowResource extends JsonResource
     {
         $data = parent::toArray($request);
 
+        // return $data;
+
         return [
-            'property_id' => $data['property']['id'],
-            'access_interface_id' => $data['id'],
-            'property_type_name' => $data['property_type']['name'],
-            'address' => $data['property']['address'],
-            'email' => $data['property']['email'],
-            'price' => $data['property']['price'],
-            'expiration_date' => $data['property']['expiration_date'],
-            'is_development' => $data['property']['is_development'],
-            'add_to_website' => $data['property']['add_to_website'],
-            'is_co_listing' => $data['property']['is_co_listing'],
-            'commission_rate' => $data['property']['commission_rate'],
-            'co_list_percentage' => $data['property']['co_list_percentage'],
-            'source' => $data['property']['source']['name'],
-            'agent' => $data['property']['agent']['first_name']. ' '. $data['property']['agent']['last_name'],
-            'co_agent' => $data['property']['co_agent']['first_name']. ' '. $data['property']['agent']['last_name'],
-            'size' => $data['size'],
-            'access_key' => $data['access_key'],
-            'lock_box_location' => $data['lock_box_location'],
-            'pickup_instructions' => $data['pickup_instructions'],
-            'gate_code' => $data['gate_code'],
-            'gete_access_location' => $data['gete_access_location'],
-            'visitor_parking' => $data['visitor_parking'],
-            'note' => $data['note'],
+            'property_id' => $data['property']['id'] ?? null,
+            'access_interface_id' => $data['id'] ?? null,
+            'property_type_name' => $data['property_type']['name'] ?? null,
+            'address' => $data['property']['address'] ?? null,
+            'email' => $data['property']['email'] ?? null,
+            'price' => $data['property']['price'] ?? null,
+            'expiration_date' => $data['property']['expiration_date'] ?? null,
+            'is_development' => $data['property']['is_development'] ?? null,
+            'add_to_website' => $data['property']['add_to_website'] ?? null,
+            'is_co_listing' => $data['property']['is_co_listing'] ?? null,
+            'commission_rate' => $data['property']['commission_rate'] ?? null,
+            'co_list_percentage' => $data['property']['co_list_percentage'] ?? null,
+            'source' => $data['property']['source']['name'] ?? null,
+            'agent' => ($data['property']['agent']['first_name'] ?? '') . ' ' . ($data['property']['agent']['last_name'] ?? ''),
+            'co_agent' => ($data['property']['co_agent']['first_name'] ?? null) && ($data['property']['co_agent']['last_name'] ?? null)
+                ? $data['property']['co_agent']['first_name'] . ' ' . $data['property']['co_agent']['last_name']
+                : 'N/A',
+            'size' => $data['size'] ?? null,
+            'access_key' => $data['access_key'] ?? null,
+            'lock_box_location' => $data['lock_box_location'] ?? null,
+            'pickup_instructions' => $data['pickup_instructions'] ?? null,
+            'gate_code' => $data['gate_code'] ?? null,
+            'gete_access_location' => $data['gete_access_location'] ?? null,
+            'visitor_parking' => $data['visitor_parking'] ?? null,
+            'note' => $data['note'] ?? null,
         ];
     }
 }

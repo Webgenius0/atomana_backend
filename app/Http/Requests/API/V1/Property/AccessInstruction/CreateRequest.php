1c7ee236c5c7daf1d04d59479368c7956bea725b
mdiktushar <md.tushar1804@gmail.com>
2025-04-10 14:25:39 +0600

    property instruction create request created

--- app/Http/Requests/API/V1/Property/AccessInstruction/CreateRequest.php
+++ app/Http/Requests/API/V1/Property/AccessInstruction/CreateRequest.php
@@ -3,7 +3,9 @@
 namespace App\Http\Requests\API\V1\Property\AccessInstruction;
 
 use App\Traits\V1\ApiResponse;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
 
 class CreateRequest extends FormRequest
 {
@@ -24,7 +26,54 @@ class CreateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "property_id"=> "required|exists:properties,id",
+            "property_types_id"=> "required|exists:property_types,id",
+            "size"=> "nullable|string",
+            "access_key"=> "nullable|string",
+            "lock_box_location"=> "nullable|string",
+            "pickup_instructions"=> "nullable|string",
+            "gate_code"=> "nullable|string",
+            "gete_access_location"=> "nullable|string",
+            "visitor_parking"=> "nullable|string",
+            "note"=> "nullable|string",
         ];
+    }
+
+    /**
+     * failedValidation
+     * @param \Illuminate\Contracts\Validation\Validator $validator
+     * @throws \Illuminate\Validation\ValidationException
+     * @return never
+     */
+    protected function failedValidation(Validator $validator): never
+    {
+        $errors = $validator->errors()->getMessages();
+        $message = null;
+        $fields = [
+            'property_id',
+            'property_types_id',
+            'size',
+            'access_key',
+            'lock_box_location',
+            'pickup_instructions',
+            'source',
+            'gete_access_location',
+            'visitor_parking',
+            'note',
+        ];
+
+        foreach ($fields as $field) {
+            if (isset($errors[$field])) {
+                $message = $errors[$field][0];
+                break;
+            }
+        }
+
+        $response = $this->error(
+            422,
+            $message,
+            $validator->errors(),
+        );
+        throw new ValidationException($validator, $response);
     }
 }

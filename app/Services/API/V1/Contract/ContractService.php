0f905d51d8057ba32be84ffd9c6d42f0aaefeb3f
mdiktushar <md.tushar1804@gmail.com>
2025-04-29 12:26:51 +0600

    create contract

--- app/Services/API/V1/Contract/ContractService.php
+++ app/Services/API/V1/Contract/ContractService.php
@@ -1,8 +1,60 @@
 <?php
-    
+
 namespace App\Services\API\V1\Contract;
+
+use App\Models\Contract;
+use App\Repositories\API\V1\Contract\ContractRepositoryInterface;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class ContractService
 {
-    // Your service logic goes here
+    /**
+     *
+     * @var ContractRepositoryInterface
+     */
+    protected ContractRepositoryInterface $contractRepository;
+    protected int $userId;
+    protected int $businessId;
+
+    /**
+     * construct
+     * @param \App\Repositories\API\V1\Contract\ContractRepositoryInterface $contractRepository
+     */
+    public function __construct(ContractRepositoryInterface $contractRepository)
+    {
+        $this->contractRepository = $contractRepository;
+        $this->userId = Auth::user()->id;
+        $this->businessId = Auth::user()->business()->id;
+    }
+
+    /**
+     * getAllContracts
+     */
+    public function getAllContracts()
+    {
+        try {
+            $perPage = request()->query('per_page');
+            return $this->contractRepository->getAllContractsByBusiness($this->businessId, $perPage);
+        } catch (Exception $e) {
+            Log::error('ContractService:getAllContracts', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
+
+    /**
+     * createContract
+     * @param array $data
+     * @return \App\Models\Contract
+     */
+    public function createContract(array $data): Contract
+    {
+        try {
+            return $this->contractRepository->createContract($data, $this->businessId, $this->userId);
+        } catch (Exception $e) {
+            Log::error('ContractService:createContract', ['error' => $e->getMessage()]);
+            throw $e;
+        }
+    }
 }

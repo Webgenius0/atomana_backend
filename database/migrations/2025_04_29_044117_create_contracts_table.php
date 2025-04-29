0fd6b3caa9ee04673addd72b68dc2fda3251cce0
mdiktushar <md.tushar1804@gmail.com>
2025-04-29 10:58:10 +0600

    contract table crated

--- database/migrations/2025_04_29_044117_create_contracts_table.php
+++ database/migrations/2025_04_29_044117_create_contracts_table.php
@@ -13,7 +13,29 @@ return new class extends Migration
     {
         Schema::create('contracts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('business_id')->constrained('businesses')->cascadeOnDelete();
+            $table->unsignedBigInteger('agent');
+            $table->string('address')->nullable();
+            $table->date('closing_data')->nullable();
+            $table->boolean('is_co_listing')->default(false);
+            $table->unsignedBigInteger('co_agent')->nullable();
+            $table->enum('represent', ['buyer', 'seller', 'both'])->default('buyer');
+            $table->date('date_listed')->nullable();
+            $table->decimal('price', 20, 2);
+            $table->date('contract_data')->nullable();
+            $table->decimal('commision_percentage');
+            $table->foreignId('property_source_id')->nullable()->constrained('property_sources')->nullOnDelete();
+            $table->string('name')->nullable();
+            $table->string('company')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('comment')->nullable();
+            
+            $table->softDeletes();
             $table->timestamps();
+
+            $table->foreign('agent')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreign('co_agent')->references('id')->on('users')->cascadeOnDelete();
         });
     }
 

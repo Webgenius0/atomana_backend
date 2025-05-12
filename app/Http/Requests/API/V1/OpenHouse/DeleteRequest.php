c4ad294b0e98ef8ed18b58206dabeceecb8d9cf8
mdiktushar <md.tushar1804@gmail.com>
2025-05-12 15:50:00 +0600

    delete request

---
+++ app/Http/Requests/API/V1/OpenHouse/DeleteRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests\API\V1\OpenHouse;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class DeleteRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id' => ['required', 'array', 'min:1'],
+            'id.*' => ['integer', 'distinct', 'exists:open_houses,id'],
+        ];
+    }
+
+    /**
+     * failedValidation
+     * @param \Illuminate\Contracts\Validation\Validator $validator
+     * @throws \Illuminate\Validation\ValidationException
+     * @return never
+     */
+    protected function failedValidation(Validator $validator): never
+    {
+        $errors = $validator->errors()->getMessages();
+        $message = null;
+        $fields = [
+            'id',
+        ];
+
+        foreach ($fields as $field) {
+            if (isset($errors[$field])) {
+                $message = $errors[$field][0];
+                break;
+            }
+        }
+
+        $response = $this->error(
+            422,
+            $message,
+            $validator->errors(),
+        );
+        throw new ValidationException($validator, $response);
+    }
+}

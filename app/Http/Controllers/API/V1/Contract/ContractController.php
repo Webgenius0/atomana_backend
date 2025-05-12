e72e1e47288574952c1cd60b1805aaffe3eb54af
mdiktushar <md.tushar1804@gmail.com>
2025-05-12 12:14:36 +0600

    index data fetch

--- app/Http/Controllers/API/V1/Contract/ContractController.php
+++ app/Http/Controllers/API/V1/Contract/ContractController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\API\V1\Contract\CreateRequest;
 use App\Services\API\V1\Contract\ContractService;
 use App\Http\Resources\API\V1\Contract\StoreResource;
 use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -22,14 +23,28 @@ class ContractController extends Controller
         $this->contractService = $contractService;
     }
 
-    public function index() {}
+    /**
+     * index
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        try {
+            $response = $this->contractService->getAllContracts();
+
+            return $this->success(201, 'contract created', $response);
+        } catch (Exception $e) {
+            Log::error('ContractController::index', ['message' => $e->getMessage()]);
+            return $this->error(500, 'server error', $e->getMessage());
+        }
+    }
 
     /**
      * store
      * @param \App\Http\Requests\API\V1\Contract\CreateRequest $createRequest
      * @return \Illuminate\Http\JsonResponse
      */
-    public function store(CreateRequest $createRequest)
+    public function store(CreateRequest $createRequest): JsonResponse
     {
         try {
             $validatedData = $createRequest->validated();

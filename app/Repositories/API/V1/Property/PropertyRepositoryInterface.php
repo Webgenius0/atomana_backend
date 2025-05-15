d773bfef3f965759b5cafe14dbb1305231dc201a
mdiktushar <md.tushar1804@gmail.com>
2025-05-15 11:42:51 +0600

    update function created

--- app/Repositories/API/V1/Property/PropertyRepositoryInterface.php
+++ app/Repositories/API/V1/Property/PropertyRepositoryInterface.php
@@ -49,4 +49,101 @@ interface PropertyRepositoryInterface
      * @return void
      */
     public function bulkDelete(array $ids): void;
+
+    /**
+     * updatePrice
+     * @param \App\Models\Property $property
+     * @param mixed $price
+     * @return void
+     */
+    public function updatePrice(Property $property, $price);
+
+    /**
+     * updateExpirationDate
+     * @param \App\Models\Property $property
+     * @param mixed $expiration_date
+     * @return void
+     */
+    public function updateExpirationDate(Property $property, $expiration_date);
+
+    /**
+     * updateCommisionRate
+     * @param \App\Models\Property $property
+     * @param mixed $commission_rate
+     * @return void
+     */
+    public function updateCommisionRate(Property $property, $commission_rate);
+
+    /**
+     * updateIsDevelopment
+     * @param \App\Models\Property $property
+     * @param mixed $is_development
+     * @return void
+     */
+    public function updateIsDevelopment(Property $property, $is_development);
+
+    /**
+     * updateWebsite
+     * @param \App\Models\Property $property
+     * @param mixed $add_to_website
+     * @return void
+     */
+    public function updateWebsite(Property $property, $add_to_website);
+
+    /**
+     * updatePropertySource
+     * @param \App\Models\Property $property
+     * @param mixed $property_source_id
+     * @return void
+     */
+    public function updatePropertySource(Property $property, $property_source_id);
+
+    /**
+     * updateBed
+     * @param \App\Models\Property $property
+     * @param mixed $beds
+     * @return void
+     */
+    public function updateBed(Property $property, $beds);
+
+    /**
+     * updateHalfBed
+     * @param \App\Models\Property $property
+     * @param mixed $half_baths
+     * @return void
+     */
+    public function updateHalfBed(Property $property, $half_baths);
+
+    /**
+     * updateFullBed
+     * @param \App\Models\Property $property
+     * @param mixed $full_baths
+     * @return void
+     */
+    public function updateFullBed(Property $property, $full_baths);
+
+    /**
+     * updateSize
+     * @param \App\Models\Property $property
+     * @param mixed $size
+     * @return void
+     */
+    public function updateSize(Property $property, $size);
+
+
+    /**
+     * updateLink
+     * @param \App\Models\Property $property
+     * @param mixed $link
+     * @return void
+     */
+    public function updateLink(Property $property, $link);
+
+    /**
+     * updateNote
+     * @param \App\Models\Property $property
+     * @param mixed $note
+     * @return void
+     */
+    public function updateNote(Property $property, $note);
 }
